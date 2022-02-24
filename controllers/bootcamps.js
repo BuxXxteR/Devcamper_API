f72c80d5b7a39adb0ba6b276fd69677f08f5cf12
Nipuna Amaranayaka <amaranayakanipuna@gmail.com>
2022-02-24 21:16:30 +0530

    new new

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,3 +1,4 @@
+const ErrorResponse = require('../utils/errorResponse');
 const Bootcamp = require('../models/Bootcamp');
 
 //@desc     Get all bootcamps
@@ -21,11 +22,16 @@ exports.getBootcamp = async (req, res, next) => {
   try {
     const bootcamp = await Bootcamp.findById(req.params.id);
     if (!bootcamp) {
-      return res.status(400).json({ success: false });
+      return new ErrorResponse(
+        `Bootcamp not found with id of ${req.params.id}`,
+        404
+      );
     }
     res.status(200).json({ success: true, data: bootcamp });
   } catch (err) {
-    res.status(400).jason({ success: false });
+    next(
+      new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+    );
   }
 };
 
