178b4fcbe6edee5f416a3e91720e40a3bf2eb8ad
Nipuna Amaranayaka <amaranayakanipuna@gmail.com>
2022-02-24 17:05:33 +0530

    code

--- server.js
+++ server.js
@@ -15,6 +15,9 @@ const bootcamps = require('./routes/bootcamps');
 
 const app = express();
 
+//Body parser
+app.use(express.json());
+
 //Dev logging middleware
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
@@ -34,7 +37,7 @@ const server = app.listen(
 
 //Handle unhandled promise rejections
 process.on('unhandledRejection', (err, promise) => {
-  console.log(`Error: ${err.message}`);
+  console.log(`Error: ${err.message}`.red);
   //Close server & exit process
   server.close(() => process.exit(1));
 });
